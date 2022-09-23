c8eb2588203565844a343bec7d0fd6eb9c0c2f04
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-09-23 14:35:46 +0100

    message

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,7 +4,7 @@ import Todo from './Todo'
 const Todos = ({transactions, deleteTransaction}) => {
   return (
     <div style={{marginTop: '20px'}}>
-      <h3  style={{textAlign: 'center'}}>Transaction History</h3>
+      <h3  style={{textAlign: 'center', marginBottom: '10px'}}>Transaction History</h3>
       {transactions.length < 1 ? 'You have no transaction' :
       <>{transactions.map(transaction=> <Todo key={transaction.id} transaction={transaction} deleteTransaction={deleteTransaction}/>)}</>}
     </div>
